697344e78628ca16c0efc4c4c65638919d72ba76
Prathik <prathik.p2002@gmail.com>
2026-04-21 18:27:48 +0530

    MapView add

--- client/src/pages/EmergencyPage.jsx
+++ client/src/pages/EmergencyPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import axios from "axios";
+import MapView from "../components/MapView";
 import "../Emergency.css";
 
 function EmergencyPage() {
@@ -12,69 +13,9 @@ function EmergencyPage() {
   const [nearbyAmbulances, setNearbyAmbulances] = useState([]);
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const [mapLoaded, setMapLoaded] = useState(false);
-
-  const mapRef = useRef(null);
-  const mapInstance = useRef(null);
-  const markersGroup = useRef(null);
 
   const API_BASE = "http://localhost:5000/api";
-
-  //loading leaflet dynamically
-  useEffect( () => {
-    if(window.L) { setMapLoaded(true); return; }
-    const link = document.createElement("link");                  //this part manually creates a <Link> tag
-    link.rel = "stylesheet";                                     //and shoves it into the <head> of the 
-    link.href = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css";//document to load leaflet css
-    document.head.appendChild(link);
-    const script = document.createElement("script");              //same for leaflet js
-    script.src = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";  
-    script.async = true;
-    script.onload = () => setMapLoaded(true);   //when leaflet is loaded, we set mapLoaded to true
-    document.head.appendChild(script);
-  },[]);
-
-  //initializes the map once emergency is assigned
-  useEffect(() => {
-    if(mapLoaded && emergency?.status === "ASSIGNED" && mapRef.current && !mapInstance.current ) {
-      const L = window.L;
-      mapInstance.current = L.map(mapRef.current).setView([lat,lng],13);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {  //fetches actual map
-        attribution : "© OpenStreetMap"
-    }).addTo(mapInstance.current);
-      markersGroup.current = L.layerGroup().addTo(mapInstance.current); // this is like a white board upadated route 
-    }                                                                  // can be drawn again without effecting map
-  }, [mapLoaded, emergency]);
-
-  //Update markers
-  useEffect( () => {
-    if(mapInstance.current && markersGroup.current && emergency?.status === "ASSIGNED") {
-      const L = window.L;
-      markersGroup.current.clearLayers(); //clear old markers
-
-      //add patient marker
-      L.marker([lat, lng], {
-        icon: L.divIcon({ html:'<div class="map-marker patient-marker">🏠</div>',
-          className: 'custom-leaflet-icon', iconSize: [32, 32], iconAnchor: [16, 32] })
-      }).addTo(markersGroup.current).bindPopup("Patient Location");
-
-      //add ambulance marker
-      if (emergency.assignedAmbulance?.location) {
-        const ambLoc = emergency.assignedAmbulance.location.coordinates;
-        L.marker([ambLoc[1], ambLoc[0]], {
-          icon: L.divIcon({ html: '<div class="map-marker ambulance-marker">🚑</div>', 
-            className: 'custom-leaflet-icon', iconSize: [32, 32], iconAnchor: [16, 16] })
-        }).addTo(markersGroup.current).bindPopup("Assigned Ambulance");
-      }
-      //add hospital marker
-      if (emergency.assignedHospital?.location) {
-        const hospLoc = emergency.assignedHospital.location.coordinates;
-        L.marker([hospLoc[1], hospLoc[0]], {
-          icon: L.divIcon({ html: '🏥', className: 'text-2xl', iconSize: [16, 30] })
-        }).addTo(markersGroup.current).bindPopup(`Hospital: ${emergency.assignedHospital.name}`);
-      }
-  }
-}, [emergency]);
+    
 
   const createEmergency = async () => {
     setLoading(true);
@@ -176,7 +117,7 @@ function EmergencyPage() {
               <p><strong>Facility:</strong> {emergency.assignedHospital?.name || "Evaluating Best ER..."}</p>
             </div>
           </div>
-          <div ref={mapRef} className="live-map-view" />
+          <MapView emergency={emergency} lat={lat} lng={lng} />
         </div>
       )}
 
